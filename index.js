f3797f5d6b5bef357c2297d1a2fb5c4c8d1793df
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-07-08 11:46:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const tutorials = [
+    "What does the this keyword mean?",
+    "What is the constructor OO pattern?",
+    "Implementing blockchain web API",
+    "The test driven development workflow",
+    "What is NaN and how can we check for it",
+    "What is the difference between stopPropagation and preventDefault?",
+    "Immutable state and pure functions",
+    "What is the difference between == and ===?",
+    "What is the difference between event capturing and bubbling?",
+    "What is JSONP?"
+  ];
+
+  const titleCasedTutorials = tutorials.map(title => {
+    return title.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+
+  return titleCasedTutorials;
 }
+
+console.log(titleCased(tutorials));
+
